aa5ad1db418324e79ce67c415561d6ba0d2d4a0c
KianSharifan <sharifankian@gmail.com>
2025-01-01 14:52:47 +0000

    Deploying to gh-pages from @ KianSharifan/kiansharifan.github.io@15aec09afb9704535bcf5cf8a66d41fa5cbbbe40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "i dont have any publication these are for cw class",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/publications/";
+            window.location.href = "/publications/";
           },
         },{id: "nav-projects",
           title: "projects",
@@ -42,7 +42,7 @@ ninja.data = [{
           description: "i havent write my cv yet but it looks like that i have to uplod something in here for cw class",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/cv/";
+            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
