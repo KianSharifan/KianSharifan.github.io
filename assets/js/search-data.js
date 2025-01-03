9924dd043ebf5a304e491b850d4fd15e00ff4405
KianSharifan <sharifankian@gmail.com>
2025-01-03 08:42:28 +0000

    Deploying to gh-pages from @ KianSharifan/kiansharifan.github.io@74b753024195aa53b8dca1cc7c1fc1ef34547292 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-images",
+            },{id: "post-a-post-with-code",
+      
+        title: "a post with code",
+      
+      description: "an example of a post with some code",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/code/";
+        
+      },
+    },{id: "post-a-post-with-images",
       
         title: "a post with images",
       
